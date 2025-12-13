6b5951e96bc1f4d32cdf1fa297e39e61409ea21e
t46d <bdcgr55qvd@privaterelay.appleid.com>
2025-12-14 00:07:09 +0400

    Update client.js

--- src/utils/supabase/client.js
+++ src/utils/supabase/client.js
@@ -1,9 +1,7 @@
-import { createBrowserClient } from '@supabase/ssr';
+import { createBrowserClient } from "@supabase/ssr";
 
-export function createClient() {
-  // يجب أن تكون هذه المتغيرات متاحة في إعدادات البيئة (Vercel Environment Variables)
-  return createBrowserClient(
+export const createClient = () =>
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   );
-}
