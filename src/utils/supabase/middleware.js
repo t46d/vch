81ec8b15a9eb3f5d329cd962b08874b4a169638d
t46d <bdcgr55qvd@privaterelay.appleid.com>
2025-12-13 22:50:23 +0400

    Update middleware.js

--- src/utils/supabase/middleware.js
+++ src/utils/supabase/middleware.js
@@ -1,26 +1,37 @@
-﻿import { createServerClient } from '@supabase/ssr'
-import { NextResponse } from 'next/server'
+import { createServerClient } from '@supabase/ssr';
+import { NextResponse } from 'next/server';
 
 export async function updateSession(request) {
-  let supabaseResponse = NextResponse.next({ request })
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
-        getAll() { return request.cookies.getAll() },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
-          supabaseResponse = NextResponse.next({ request })
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
-          )
+        get: (name) => request.cookies.get(name)?.value,
+        set: (name, value, options) => {
+          request.cookies.set({ name, value, ...options });
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          supabaseResponse.cookies.set({ name, value, ...options });
+        },
+        remove: (name, options) => {
+          request.cookies.set({ name, value: '', ...options });
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          supabaseResponse.cookies.set({ name, value: '', ...options });
         },
       },
     }
-  )
+  );
 
-  await supabase.auth.getUser() // تحديث الجلسة تلقائياً
-  return supabaseResponse
+  // تحديث الجلسة عن طريق الوصول إلى البيانات
+  await supabase.auth.getUser();
+
+  return supabaseResponse;
 }
