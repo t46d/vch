748b9eeda3df89c6941ec46c0705e63401c71819
t46d <bdcgr55qvd@privaterelay.appleid.com>
2025-12-18 17:10:19 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
@@ -16,6 +19,10 @@ const nextConfig = {
       use: ['@svgr/webpack'],
     });
     return config;
+  },
+  // إضافة هذا لحل مشاكل Edge Runtime مع Supabase
+  experimental: {
+    serverComponentsExternalPackages: ['@supabase/supabase-js'],
   },
 };
 
